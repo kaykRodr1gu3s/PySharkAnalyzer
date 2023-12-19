4427e6b3f2c429305737bd40c305d31db38c2fcf
Kayk Rodrigues <Kaykcesar99@gmail.com>
2023-12-19 12:01:37 -0300

    modified:   Pyshark.py

--- Pyshark.py
+++ Pyshark.py
@@ -1,24 +1,46 @@
 import pyshark
-
+from datetime import datetime
 
 def layer_ip(file ='./2023-12-15-TA577-Pikabot-infection-traffic.pcap'):
     pyshark_obj = pyshark.FileCapture(file)
-    src_dst_proto_proto = {}
+    src_dst = {}
     source = []
     destination = []
-    protocol = []
+    
     
     for packet in pyshark_obj:   
        try:
             source.append(packet.ip.src)
             destination.append(packet.ip.dst)
-            protocol.append(packet.transport_layer)
        
        except: 
             continue
        
-    src_dst_proto_proto['Source'] = source
-    src_dst_proto_proto['Destination'] = destination
-    src_dst_proto_proto['Protocol'] = protocol
+    src_dst['Source'] = source
+    src_dst['Destination'] = destination
 
-    return src_dst_proto_proto
+    return src_dst
+
+
+def protocol(file = './2023-12-15-TA577-Pikabot-infection-traffic.pcap'):
+     protocol = {}
+     protocol_name = []
+     pyshark_obj = pyshark.FileCapture(file)
+
+     for packet in pyshark_obj:
+          
+          protocol_name.append(packet.transport_layer)
+
+     protocol['Protocol'] = protocol_name
+
+     return protocol
+       
+
+def epoch_time(file = './2023-12-15-TA577-Pikabot-infection-traffic.pcap'):
+     pyshark_obj = pyshark.FileCapture(file)
+     for c in pyshark_obj:
+         epoch = float(c.frame_info.time_epoch)
+         date_time = datetime.fromtimestamp(epoch)
+         print(date_time)
+         input()
+
